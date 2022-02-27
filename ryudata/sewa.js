a53aef50a4f3088b32b078b801c2c9b15b9fbc77
NekoKyun <91820453+Yuri-Neko@users.noreply.github.com>
2022-02-27 18:05:49 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -3,9 +3,8 @@ exports.sewa = () => {
 
 ┏━━━━━━
 ┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
-┃ *1 MINGGU : Rp 5.000*
-┃ *1 BULAN : Rp 15.000*
-┃ *PERMANEN : Rp 35.000*
+┃ *2 MINGGU : Rp 5.000*
+┃ *PERMANEN : Rp 15.000*
 ┗━━━━━━
 ┏━━━━━━
 ┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
@@ -18,6 +17,6 @@ exports.sewa = () => {
 *SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
 
 *MINAT? CHAT :
-wa.me/6285609233482
+wa.me/6282112080081
 `
 }
